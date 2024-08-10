f018f5af6adff5484275fd76b697fc14001b9ee5
rodi38 <rodrigorochaed1@gmail.com>
2024-08-09 22:39:49 -0300

    feat: api

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,11 +1,29 @@
-const API = "http://localhost:3333";
+import { IFilter } from "../types/IFilterTask";
+import { ITask } from "../types/Task";
+
+const API = "http://localhost:5000";
 
 const endpoint = (path: string): string => API + path;
 
-const get = async (path: string): Promise<any> => {
-  return fetch(endpoint(path)).then((res) => res.json());
+const get = async (path: string, payload?: IFilter): Promise<any> => {
+  return fetch(endpoint(path), {method: "GET", body: JSON.stringify(payload)}).then((res) => res.json());
+};
+const post = async (path: string, payload: ITask): Promise<ITask> => {
+  return fetch(endpoint(path), {method: "POST", body: JSON.stringify(payload)}).then((res) => res.json());
 };
 
-export const getVehicles = async () => {
-  return get("/vehicles");
+const put = async (path: string, payload: ITask): Promise<ITask> => {
+  return fetch(endpoint(path), {method: "PUT", body: JSON.stringify(payload)}).then((res) => res.json());
+};
+
+
+export const getTasks = async (payload?: IFilter) => {
+  return get("/tasks", payload);
+};
+
+export const postTask = async (payload: ITask) => {
+  return post("/tasks", payload);
+};
+export const putTask = async (id: string, payload: ITask) => {
+  return put(`/tasks/${id}`, payload);
 };
