2b6ca738c9c5adbedf4fc543c2200f3fa5b29692
rodi38 <rodrigorochaed1@gmail.com>
2024-08-12 16:37:12 -0300

    refactor: simple header component refactor

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,6 +2,9 @@ import styles from "./Header.module.scss"
 import Search from "../Search";
 import logo from "../../assets/logo.png"
 import close from "../../assets/close.png"
+import { IoIosMenu } from "react-icons/io";
+import { useState } from "react";
+
 
 interface IHeader {
   value: string;
@@ -10,38 +13,62 @@ interface IHeader {
 }
 
 const Header = (props: IHeader) => {
+  const [toggleMenuHeader, setToggleMenuHeader] = useState(true);
+
+  const handlerToggleMenu = () => {
+    console.log(toggleMenuHeader);
+
+    setToggleMenuHeader(!toggleMenuHeader);
+
+  }
   const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
     props.onChange(e);
     props.fetchSearchedTasks({ title: e.target.value });
   }
 
   return (
-    <header className={styles.HeaderPrincipal}>
-      <div className={styles.Header}>
+    <>
+      {toggleMenuHeader ?
+        <header className={styles.HeaderPrincipal}>
 
-        <div className={styles.headerLogo}>
-          <img
-            src={logo}
-            alt="Descrição da Imagem"
-            style={{ width: '2.269rem', height: '2.269rem' }}
-          />
-        </div>
-        <div className={styles.headerParagraph}>
-          <p>CoreNotes</p>
-        </div>
-        <div className={styles.headerSearch}>
-          <Search placeholder="Pesquisar notas" value={props.value} onChange={handleSearch} />
-        </div>
-      </div>
-      <div className={styles.headerX}>
-        <img
-          src={close}
-          alt="Descrição da Imagem"
-          className={styles.closeCard}
-        />
-      </div>
+          <div className={styles.Header}>
+            <div className={styles.headerLogo}>
+              <img
+                src={logo}
+                alt="Descrição da Imagem"
+                style={{ width: '2.269rem', height: '2.269rem' }}
+              />
+            </div>
+            <div className={styles.headerParagraph}>
+              <p>CoreNotes</p>
+            </div>
+            <div className={styles.headerSearch}>
+              <Search placeholder="Pesquisar notas" value={props.value} onChange={handleSearch} />
+            </div>
+          </div>
 
-    </header>
+          <div className={styles.headerX}>
+            <img
+              src={close}
+              alt="Descrição da Imagem"
+              className={styles.closeCard}
+              onClick={handlerToggleMenu}
+            />
+
+          </div>
+
+        </header>
+
+        :
+
+        <div className={styles.containerHambuguerMenu}>
+          <IoIosMenu className={styles.burguermenu} onClick={handlerToggleMenu}></IoIosMenu>
+
+        </div>
+
+      }
+    </>
+
 
   );
 };
